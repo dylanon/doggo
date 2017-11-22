2a94caa1173e1d0fb2d4322f10e24280e162df49
Dylan On <dylanon@users.noreply.github.com>
2017-11-22 17:59:59 -0500

    Make CreateActionForm and App stateful

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -2,17 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
-        return (
-          <div>
-            <Header />
-            <main>
-              <CreateActionForm />
-              <ActionList />
-            </main>
-          </div>
-        )
+  constructor() {
+    super();
+    this.state = {
+      actions: []
     }
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <main>
+          <CreateActionForm />
+          <ActionList />
+        </main>
+      </div>
+    )
+  }
 }
 
 const Header = () => {
@@ -24,15 +30,38 @@ const Header = () => {
 }
 
 class CreateActionForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      actionName: '',
+      actionDescription: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    console.log(e.target.name);
+    const inputName = e.target.name;
+    const inputValue = e.target.value;
+    this.setState({
+      [inputName]: inputValue
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('clicked');
+  }
+
   render() {
     return(
       <div>
         <h2>Create an action</h2>
-        <form action="" id="create-action">
-          <label htmlFor="new-action-name">Name:</label>
-          <input type="text" id="new-action-name"/>
+        <form action="" id="create-action" onSubmit={this.handleSubmit}>
+          <label htmlFor="new-action-name">Action Name:</label>
+          <input type="text" id="new-action-name" name="actionName" onChange={this.handleChange} />
           <label htmlFor="new-action-description">Description (optional):</label>
-          <input type="text" id="new-action-description"/>
+          <input type="text" id="new-action-description" name="actionDescription" onChange={this.handleChange} />
           <button type="submit">Create</button>
         </form>
       </div>
