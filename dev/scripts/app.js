faff790cedac99e04e41c0dd5ceac5b5aa763a46
Dylan On <dylanon@users.noreply.github.com>
2017-11-22 21:55:00 -0500

    Add firebase init

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 
 class App extends React.Component {
   constructor() {
@@ -8,6 +9,19 @@ class App extends React.Component {
       actions: []
     }
     this.addAction = this.addAction.bind(this);
+  }
+
+  componentDidMount() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyALWamD34pm4LMj-YUxKDDUHEbBqn-390E",
+      authDomain: "doglog-fc202.firebaseapp.com",
+      databaseURL: "https://doglog-fc202.firebaseio.com",
+      projectId: "doglog-fc202",
+      storageBucket: "",
+      messagingSenderId: "900360389303"
+    };
+    firebase.initializeApp(config);
   }
 
   addAction(name, description) {
