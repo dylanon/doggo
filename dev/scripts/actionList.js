47b58915e150d2ebc6a9282621a0e0e2f4cc5227
Dylan On <dylanon@users.noreply.github.com>
2017-11-23 16:45:01 -0500

    Push action details and timestamp to database on action click

--- dev/scripts/actionList.js
+++ dev/scripts/actionList.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import firebase from './firebase';
-import {actionsRef} from './firebase'
 
 class ActionList extends React.Component {
+  handleClick(actionID, actionName) {
+    // Push timestamp, action id, and action name to log in firebase
+    const logRef = firebase.database().ref('users/dylanon/log');
+    const timestamp = new Date();
+    logRef.push({
+      actionName: actionName,
+      actionID: actionID,
+      timestamp: firebase.database.ServerValue.TIMESTAMP
+    });
+    // Change last completed to 'Just now'
+  }
+
   handleDelete(e) {
     e.preventDefault();
     const deleteID = e.target.getAttribute('data-delete');
@@ -20,7 +31,8 @@ class ActionList extends React.Component {
               <li key={action.key}>
                 <p>{action.name} | <span>{action.description}</span></p>
                 <p>Last completed: Yesterday at 7:20am</p>
-                <a href="#" id="delete-action" data-delete={action.key} onClick={this.handleDelete}>Delete</a>
+                <a href="#" id="log-action" onClick={() => {this.handleClick(action.key, action.name)}}>Complete Action (add to log)</a> | 
+                <a href="#" id="delete-action" data-delete={action.key} onClick={this.handleDelete}>(Delete)</a>
               </li>
             )
           })}
