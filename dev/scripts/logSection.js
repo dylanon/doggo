4cf54fa5cfa3a7d23f74304f9b214746ae5b750c
Dylan On <dylanon@users.noreply.github.com>
2017-11-23 22:46:11 -0500

    Make LogSection a simple component

--- dev/scripts/logSection.js
+++ dev/scripts/logSection.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import moment from 'moment';
 
-class LogSection extends React.Component {
-    render() {
-        const date = this.props.date;
-        const logArray = this.props.log;
+const LogSection = (props) => {
+    const date = props.date;
+    const logArray = props.log;
 
-        // Create today's heading
-        const dateHeading = moment(date).format("MMMM Do, YYYY")
+    // Create today's heading
+    const dateHeading = moment(date).format("MMMM Do, YYYY");
 
-        // Grab only today's logs
-        const todaysLogs = logArray.filter((entry) => {
-            return entry.date === date;
-        })
+    // Grab only today's logs
+    const todaysLogs = logArray.filter((entry) => {
+        return entry.date === date;
+    })
 
-        return (
-            <div>
-                <h3>{dateHeading}</h3>
-                <ul>
-                    {todaysLogs.map((entry) => {
-                        return <LogItem entry={entry}/>
-                    })}
-                </ul>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h3>{dateHeading}</h3>
+            <ul>
+                {todaysLogs.map((entry) => {
+                    return <LogItem entry={entry}/>
+                })}
+            </ul>
+        </div>
+    )
 }
 
 const LogItem = (props) => {
