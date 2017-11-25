d862f6f9cd4868d8b9c13ec2695503c6e1b1722c
Dylan On <dylanon@users.noreply.github.com>
2017-11-25 17:33:30 -0500

    Change to popup sign-in

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -20,6 +20,7 @@ class App extends React.Component {
     }
     this.filterLog = this.filterLog.bind(this);
     this.resetFilter = this.resetFilter.bind(this);
+    this.logIn = this.logIn.bind(this);
   }
 
   filterLog(actionID, actionName) {
@@ -37,56 +38,24 @@ class App extends React.Component {
   logIn(e) {
     e.preventDefault();
     const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithRedirect(provider);
-    // firebase.auth().signInWithPopup(provider).then(function(result) {
-    //   // This gives you a Google Access Token. You can use it to access the Google API.
-    //   var token = result.credential.accessToken;
-    //   // The signed-in user info.
-    //   var user = result.user;
-    //   // ...
-    //   console.log(`signed in`);
-    //   console.log(user);
-    // }).catch(function(error) {
-    //   // Handle Errors here.
-    //   var errorCode = error.code;
-    //   var errorMessage = error.message;
-    //   // The email of the user's account used.
-    //   var email = error.email;
-    //   // The firebase.auth.AuthCredential type that was used.
-    //   var credential = error.credential;
-    //   // ...
-    // });
+    firebase.auth().signInWithPopup(provider)
+    .then((result) => {
+      this.setState({
+        loggedIn: true,
+        userID: result.user.uid
+      });
+    });
   }
 
   logOut(e) {
     e.preventDefault();
-    firebase.auth().signOut().then(function() {
+    firebase.auth().signOut()
+    .then(function() {
       console.log('sign out successful');
-    }).catch(function(error) {
-      console.log('oops - signing out did not go so well. try again.');
     });
   }
 
   componentDidMount() {
-    // // Get sign-in info after redirect
-    // firebase.auth().getRedirectResult().then(function(result) {
-    //   if (result.credential) {
-    //     // This gives you a Google Access Token. You can use it to access the Google API.
-    //     var token = result.credential.accessToken;
-    //     // ...
-    //   }
-    //   // The signed-in user info.
-    //   var user = result.user;
-    // }).catch(function(error) {
-    //   // Handle Errors here.
-    //   var errorCode = error.code;
-    //   var errorMessage = error.message;
-    //   // The email of the user's account used.
-    //   var email = error.email;
-    //   // The firebase.auth.AuthCredential type that was used.
-    //   var credential = error.credential;
-    //   // ...
-    // });
     
     // Define a function to update log in state
     const setLogInState = (logInBoolean, userID) => {
