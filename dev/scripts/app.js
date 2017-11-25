2d8fc87d63f706de4b67f4c1abe05827a72119d3
Dylan On <dylanon@users.noreply.github.com>
2017-11-24 21:13:48 -0500

    Centralize state in App component

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import firebase from './firebase';
 import {actionsRef} from './firebase'
+import moment from 'moment';
 import Header from './header';
 import ActionForm from './actionForm';
 import ActionList from './actionList';
@@ -11,8 +12,25 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      actions: []
+      actions: [],
+      log: [],
+      filterBy: '',
+      filterByName: ''
     }
+    this.filterLog = this.filterLog.bind(this);
+    this.resetFilter = this.resetFilter.bind(this);
+  }
+
+  filterLog(actionID, actionName) {
+    this.setState({
+        filterBy: actionID,
+        filterByName: actionName
+    });
+  }
+
+  resetFilter(e) {
+    e.preventDefault();
+    this.filterLog('');
   }
 
   componentDidMount() {
@@ -34,6 +52,33 @@ class App extends React.Component {
         actions: newState
       });
     });
+
+    // Set the database reference
+    const logRef = firebase.database().ref('users/dylanon/log');
+    
+    // Load log data and listen for changes
+    logRef.on('value', (snapshot) => {
+        const retrievedLog = snapshot.val();
+        const tempLog = [];
+        for (let item in retrievedLog) {
+            const logItem = {
+                logID: item,
+                actionID: retrievedLog[item].actionID,
+                actionName: retrievedLog[item].actionName,
+                timestamp: retrievedLog[item].timestamp,
+                date: moment(retrievedLog[item].timestamp).format('YYYY-MM-DD')
+            }
+            tempLog.push(logItem);
+        }
+
+        // Sort by timestamp (reverse chronological order)
+        tempLog.sort((a, b) => b.timestamp - a.timestamp);
+
+        // Update the state
+        this.setState({
+            log: tempLog,
+        });
+    });
   }
 
   render() {
@@ -43,7 +88,7 @@ class App extends React.Component {
         <main>
           <ActionForm />
           <ActionList storedActions={this.state.actions} />
-          <Log />
+          <Log log={this.state.log} filterBy={this.state.filterBy} filterByName={this.state.filterByName} filterLog={this.filterLog} resetFilter={this.resetFilter}/>
         </main>
       </div>
     )
--- dev/scripts/log.js
+++ dev/scripts/log.js
@@ -4,65 +4,13 @@ import moment from 'moment';
 import LogSection from './logSection';
 
 class Log extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            log: [],
-            filterBy: '',
-            filterByName: ''
-        }
-        this.filterLog = this.filterLog.bind(this);
-        this.resetFilter = this.resetFilter.bind(this);
-    }
-
-    filterLog(actionID, actionName) {
-        this.setState({
-            filterBy: actionID,
-            filterByName: actionName
-        });
-    }
-
-    resetFilter(e) {
-        e.preventDefault();
-        this.filterLog('');
-    }
-
-    componentDidMount() {
-        // Set the database reference
-        const logRef = firebase.database().ref('users/dylanon/log');
-
-        // Load log data and listen for changes
-        logRef.on('value', (snapshot) => {
-            const retrievedLog = snapshot.val();
-            const tempLog = [];
-            for (let item in retrievedLog) {
-                const logItem = {
-                    logID: item,
-                    actionID: retrievedLog[item].actionID,
-                    actionName: retrievedLog[item].actionName,
-                    timestamp: retrievedLog[item].timestamp,
-                    date: moment(retrievedLog[item].timestamp).format('YYYY-MM-DD')
-                }
-                tempLog.push(logItem);
-            }
-
-            // Sort by timestamp (reverse chronological order)
-            tempLog.sort((a, b) => b.timestamp - a.timestamp);
-
-            // Update the state
-            this.setState({
-                log: tempLog,
-            });
-        });
-    }
-
     render() {
         // Get the array of log items
-        let logArray = Array.from(this.state.log);
+        let logArray = Array.from(this.props.log);
         // If there is an action to filter by, filter only those that match the action ID
-        if (this.state.filterBy.length > 0) {
+        if (this.props.filterBy.length > 0) {
             logArray = logArray.filter((entry) => {
-                return entry.actionID === this.state.filterBy;
+                return entry.actionID === this.props.filterBy;
             });
         }
         // Store each unique calendar date
@@ -74,20 +22,20 @@ class Log extends React.Component {
         });
         // Build the log heading text
         let logHeading = 'Log';
-        if (this.state.filterBy.length > 0) {
-            logHeading = logHeading + `: ${this.state.filterByName}`;
+        if (this.props.filterBy.length > 0) {
+            logHeading = logHeading + `: ${this.props.filterByName}`;
         }
         // Display the markup
         return (
             <div>
                 <h2>{logHeading}</h2>
                 {/* If the log has been filtered, show a link to reset */}
-                {this.state.filterBy.length > 0 &&
-                    <a href="#" onClick={this.resetFilter}>Display all actions</a>
+                {this.props.filterBy.length > 0 &&
+                    <a href="#" onClick={this.props.resetFilter}>Display all actions</a>
                 }
                 {/* Map over the array of unique dates, display a section for each */}
                 {uniqueDates.map((date) => {
-                    return <LogSection key={date} date={date} log={logArray} filterFunction={this.filterLog} filterBy={this.state.filterBy}/>
+                    return <LogSection key={date} date={date} log={logArray} filterFunction={this.props.filterLog} filterBy={this.props.filterBy}/>
                 })}
             </div>
         )
