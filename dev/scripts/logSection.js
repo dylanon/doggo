59daa68a72f41ace003115df5ab482654c53237d
Dylan On <dylanon@users.noreply.github.com>
2017-11-26 16:34:25 -0500

    Filter log entries by action by tapping a log entry. Removed instructions and attached click listener to the log card.

--- dev/scripts/logSection.js
+++ dev/scripts/logSection.js
@@ -41,6 +41,7 @@ class LogItem extends React.Component {
 
     handleDelete(e) {
         e.preventDefault();
+        e.stopPropagation();
         // Set up variables
         const logID = this.props.entry.logID;
         const entryRef = firebase.database().ref(`users/${this.props.userID}/log/${logID}`);
@@ -81,11 +82,8 @@ class LogItem extends React.Component {
     render() {
         const entry = this.props.entry;
         return (
-            <li className="log-card">
+            <li className="log-card" onClick={this.handleClick}>
                 <p>You completed: <span className="text-accent">{entry.actionName}</span></p>
-                {this.props.filterBy.length === 0 &&
-                    <p><a href="#" onClick={this.handleClick}>View history for this action only</a></p>
-                }
                 <p className="log-timestamp">{moment(entry.timestamp).format('h:mm a')}</p>
                 <div className="delete-control">
                     <a href="#" onClick={this.handleDelete} aria-label="Delete this entry permanently" title="Delete permanently"><i className="fa fa-times" aria-hidden="true"></i></a>
