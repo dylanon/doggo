edb4a6716df083ec6b8fb854562be5cad52e22d5
Dylan On <dylanon@users.noreply.github.com>
2018-01-08 12:51:54 -0500

    If no history exists, guide the user back to the Actions view

--- dev/scripts/log.js
+++ dev/scripts/log.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import LogSection from './logSection';
+import { Link } from 'react-router-dom';
 
 class Log extends React.Component {
+    componentWillUnmount() {
+        // Clear log filter so it's unfiltered when we return
+        this.props.filterLog('', '');
+    }
+
     render() {
         // Get the array of log items
         let logArray = Array.from(this.props.log);
@@ -23,6 +29,29 @@ class Log extends React.Component {
         if (this.props.filterBy.length > 0) {
             logHeading = logHeading + `: ${this.props.filterByName}`;
         }
+        //
+        let content;
+        if (this.props.log.length > 0) {
+            content = (
+                <div>
+                    {/* Map over the array of unique dates, display a section for each */}
+                    {uniqueDates.map((date) => {
+                        return <LogSection key={date} date={date} log={logArray} userID={this.props.userID} filterFunction={this.props.filterLog} filterBy={this.props.filterBy}/>
+                    })}
+                </div>
+            );
+        } else {
+            let isFiltered = false;
+            if (this.props.filterBy.length > 0) {
+                isFiltered = true;
+            }
+            content = (
+                <div>
+                    <p>Oops! Looks like you haven't completed {isFiltered ? 'this action' : 'any actions'} yet.</p>
+                    <p>Go to your <Link to='/'>Actions </Link> to complete {isFiltered ? 'it' : 'one'}.</p>
+                </div>
+            )
+        }
         // Display the markup
         return (
             <div className="log-container">
@@ -31,10 +60,7 @@ class Log extends React.Component {
                 {this.props.filterBy.length > 0 &&
                     <a href="#" onClick={this.props.resetFilter}>View full history (all actions)</a>
                 }
-                {/* Map over the array of unique dates, display a section for each */}
-                {uniqueDates.map((date) => {
-                    return <LogSection key={date} date={date} log={logArray} userID={this.props.userID} filterFunction={this.props.filterLog} filterBy={this.props.filterBy}/>
-                })}
+                {content}
             </div>
         )
     }
